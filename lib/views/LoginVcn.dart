b34416690d5155a704e075b91054daddccb33597
huangchen <huangchen@huangchendeMac-mini.local>
2018-12-18 17:43:25 +0800

    修改登录页面

--- lib/views/LoginVcn.dart
+++ lib/views/LoginVcn.dart
@@ -6,6 +6,12 @@ class LoginVcn extends StatefulWidget{
 }
 
 class _LoginVcnState extends State<LoginVcn>{
+  String phoneNum;
+  String passwordNum;
+  // 手机号码的控制器
+  final TextEditingController _phonecontroller = new TextEditingController();
+  // 密码输入的控制器
+  final TextEditingController _passController = new TextEditingController();
   @override
   void initState(){
     super.initState();
@@ -13,11 +19,12 @@ class _LoginVcnState extends State<LoginVcn>{
   @override
   Widget build(BuildContext context){
      return new Scaffold(
-         body: new Center(
+         body: new Container(
             child: new ListView(
                scrollDirection: Axis.vertical,
                children: <Widget>[
                    _topItem(),
+                   _inputItem(),
                ],
             ),
          ),
@@ -42,8 +49,63 @@ class _LoginVcnState extends State<LoginVcn>{
         ),
       );
   }
+  
+  Widget _inputItem(){
+     return new Container(
+        padding: const EdgeInsets.all(20),
+        child: new Column(
+           children: <Widget>[
+             new Row(
+                children: <Widget>[
+                  new Align(
+                      alignment: FractionalOffset.centerLeft,
+                      child: new Text('手机号码', style: new TextStyle(color: const Color(0xff333333),fontSize: 14,),),
+                  ),
+                  new TextField(
+                    controller: _phonecontroller,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: '请输入手机号',
+                      icon: Icon(Icons.phone)
+                    ),
+                  )
+                ],
+             ),
+           ],
+        ),
+     );
+  }
+  
+  _textChange(String text){
+    print(text);
+  }
 
   _closeWindow(){
      Navigator.pop(context);
+  }
+
+  _login(){
+    if (_phonecontroller.text.length != 11) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('手机号码格式不对'),
+              ));
+    } else if (_passController.text.length == 0) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('请填写密码'),
+              ));
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('登录成功'),
+              ));
+      _phonecontroller.clear();        
+      _phonecontroller.clear();
+    }
   }
 }
